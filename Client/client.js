f29c1216d5b816d826ccb2da32749430afee696b
Walt Ostrander <waltostrander@gmail.com>
2023-03-04 10:44:46 -0600

    played around with JWTs for authentication but decided bcrypt/hashing/salting was easier. Updated client side code accordingly.

--- Client/client.js
+++ Client/client.js
@@ -1,40 +1,54 @@
 const axios = require("axios");
 
 // Client-side code to submit a new question to the database
-document
-  .querySelector("#submit-question")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
+document.querySelector("#submit-question").addEventListener("click", function(event) {
+  event.preventDefault();
+  
+  const nameInput = document.querySelector("#name").value;
+  const questionInput = document.querySelector("#question").value;
+  const categoryInput = document.querySelector("#category").value;
 
-    const nameInput = document.querySelector("#name").value;
-    const questionInput = document.querySelector("#question").value;
-    const categoryInput = document.querySelector("#category").value;
+  // Making a post request to the server to submit the new question to the database
+  axios
+    .post("/submitQuestion", {
+      name: nameInput,
+      question: questionInput,
+      category: categoryInput
+    })
+    .then(response => {
+      console.log(response.data);
+      // Clearing the form fields after successful submission
+      document.querySelector("#name").value = "";
+      document.querySelector("#question").value = "";
+      document.querySelector("#category").value = "";
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
-    // Get the JWT token from localStorage
-    const token = localStorage.getItem("token");
+// Client-side code to handle user authentication
+document.querySelector("#login-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  const usernameInput = document.querySelector("#username").value;
+  const passwordInput = document.querySelector("#password").value;
 
-    // Add the JWT token to the headers
-    const headers = { Authorization: `Bearer ${token}` };
-
-    // Making a post request to the server to submit the new question to the database
-    axios
-      .post(
-        "/submitQuestion",
-        {
-          name: nameInput,
-          question: questionInput,
-          category: categoryInput,
-        },
-        { headers }
-      )
-      .then((response) => {
-        console.log(response.data);
-        // Clearing the form fields after successful submission
-        document.querySelector("#name").value = "";
-        document.querySelector("#question").value = "";
-        document.querySelector("#category").value = "";
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  });
+  // Making a post request to the server to authenticate the user
+  axios
+    .post("/login", {
+      username: usernameInput,
+      password: passwordInput
+    })
+    .then(response => {
+      console.log(response.data);
+      // Storing the user ID and access token in localStorage
+      localStorage.setItem("userId", response.data.userId);
+      localStorage.setItem("accessToken", response.data.accessToken);
+      // Redirecting to the dashboard page after successful login
+      window.location.href = "/dashboard";
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
