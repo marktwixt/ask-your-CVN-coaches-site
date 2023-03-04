ae9a874c4aa2b888f0719e69338b191699b87d9c
Walt Ostrander <waltostrander@gmail.com>
2023-03-04 10:13:43 -0600

    updated db schema and server file accordingly, and updated client side JS code to handle authorization using JWT tokens

--- Client/client.js
+++ Client/client.js
@@ -1,28 +1,40 @@
 const axios = require("axios");
 
 // Client-side code to submit a new question to the database
-document.querySelector("#submit-question").addEventListener("click", function(event) {
-  event.preventDefault();
-  
-  const nameInput = document.querySelector("#name").value;
-  const questionInput = document.querySelector("#question").value;
-  const categoryInput = document.querySelector("#category").value;
+document
+  .querySelector("#submit-question")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
 
-  // Making a post request to the server to submit the new question to the database
-  axios
-    .post("/submitQuestion", {
-      name: nameInput,
-      question: questionInput,
-      category: categoryInput
-    })
-    .then(response => {
-      console.log(response.data);
-      // Clearing the form fields after successful submission
-      document.querySelector("#name").value = "";
-      document.querySelector("#question").value = "";
-      document.querySelector("#category").value = "";
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
+    const nameInput = document.querySelector("#name").value;
+    const questionInput = document.querySelector("#question").value;
+    const categoryInput = document.querySelector("#category").value;
+
+    // Get the JWT token from localStorage
+    const token = localStorage.getItem("token");
+
+    // Add the JWT token to the headers
+    const headers = { Authorization: `Bearer ${token}` };
+
+    // Making a post request to the server to submit the new question to the database
+    axios
+      .post(
+        "/submitQuestion",
+        {
+          name: nameInput,
+          question: questionInput,
+          category: categoryInput,
+        },
+        { headers }
+      )
+      .then((response) => {
+        console.log(response.data);
+        // Clearing the form fields after successful submission
+        document.querySelector("#name").value = "";
+        document.querySelector("#question").value = "";
+        document.querySelector("#category").value = "";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
