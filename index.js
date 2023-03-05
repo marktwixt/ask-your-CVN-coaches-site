5e11855c0f3dcb7205bc92fabf3d971e78d86291
Walt Ostrander <waltostrander@gmail.com>
2023-03-05 11:03:56 -0600

    added CSS styling around registration forms

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Joi = require('joi');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
